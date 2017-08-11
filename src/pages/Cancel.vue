4313e1e88bf493bd1543c41188ebb77352bec331
lihong0211 <1419686503@qq.com>
2017-08-11 20:20:29 +0800

    asdf

---
+++ src/pages/Cancel.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<swipe />
+	</div>
+</template>
+
+<script>
+
+	import Swipe from '@/components/Swipe'
+
+	export default {
+
+		data () {
+			return {
+
+			}
+		},
+
+		components: {
+			'swipe': Swipe
+		},
+
+		created () {
+			document.title = '健身助手';
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+	.test {
+		color: red;
+	}
+</style>
---
+++ src/pages/Vip.vue
@@ -0,0 +1,38 @@
+<!-- 我的会员卡 -->
+<template>
+	<div>
+		<swipe />
+	</div>
+	<div class="test"></div>
+</template>
+
+<script>
+
+	import Swipe from '@/components/Swipe'
+
+	export default {
+
+		data () {
+			return {
+
+			}
+		},
+
+		components: {
+			'swipe': Swipe
+		},
+
+		created () {
+			document.title = '健身助手';
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+	.test {
+		width:100%;
+		height:4rem;
+		background:linear-gradient(to right,pink,blue);
+	}
+
+</style>
